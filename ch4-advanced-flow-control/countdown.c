a953af6a713bc502ffe6f4630df840f854d0a9f7
mwndrgl-dev <marcwanduragala@gmail.com>
2025-03-24 00:11:27 +0100

    Add Chapter 4 Exercises : breakloop.c, countdown.c, evenumbers.c, forloop.c and switch.c

---
+++ ch4-advanced-flow-control/countdown.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+	int a;
+
+	for (a = 10; a >= 1; a--)
+	{
+		printf("a is equal to %d\n", a);
+	}
+	return 0;
+}
---
+++ ch4-advanced-flow-control/evennumbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+	int a;
+
+	for (a = 2; a <= 10; a += 2)
+	{
+		printf("a is equal to %d\n", a);
+	}
+	return 0;
+}
